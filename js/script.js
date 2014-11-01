e0fb3a6890a3baf1bcf3dee209e87e06150b4d9f
Will Gallop <wgallop99@gmail.com>
2014-11-01 11:41:59 -0400

    cleaning up jquery

--- js/script.js
+++ js/script.js
@@ -1,50 +1,34 @@
-$( document ).ready(function() {
-
-  // $(window).scroll( function() {
-  //   var value = $(this).scrollTop();
-  //   if ( value > 1451 )
-  //       $(".workHeading").css("display", "none");
-  // });
+$(document).ready(function() {
 
   $(window).on('scroll', function() {
     console.log( $(this).scrollTop() );
   });
 
   $('.homeTag').on('click', 'a', function () {
-    console.log("asdfasdfa");
-    event.preventDefault();
         $(".body").ScrollTo({
             duration: 1000
         });
     });
 
   $('.workTag').on('click', 'a', function () {
-    console.log("asdfasdfa");
-    event.preventDefault();
         $('#work').ScrollTo({
             duration: 1000
         });
     });
 
   $('.aboutTag').on('click', 'a', function () {
-    console.log("asdfasdfa");
-    event.preventDefault();
         $('.aboutHeader').ScrollTo({
             duration: 1000
         });
     });
 
     $('.contactTag').on('click', 'a', function () {
-      console.log("asdfasdfa");
-      event.preventDefault();
           $('.contact').ScrollTo({
               duration: 1000
           });
     });
 
     $('.downWrap').on('click', 'a', function () {
-      console.log("asdfasdfa");
-      event.preventDefault();
           $('#work').ScrollTo({
               duration: 1000
           });
