b5fc83258b64fd6cd54f426a2e21a2b61e19c8ff
Will Gallop <wgallop99@gmail.com>
2014-11-01 12:58:51 -0400

    removed unnecessary jquery plugin and cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,37 +1,28 @@
 $(document).ready(function() {
 
-  $(window).on('scroll', function() {
-    console.log( $(this).scrollTop() );
-  });
-
   $('.homeTag').on('click', 'a', function () {
-        $(".body").ScrollTo({
-            duration: 1000
-        });
+    $('html, body').animate({
+        scrollTop: $('body').offset().top }, 1000);
     });
 
   $('.workTag').on('click', 'a', function () {
-        $('#work').ScrollTo({
-            duration: 1000
-        });
+    $('html, body').animate({
+        scrollTop: $('#work').offset().top }, 1000);
     });
 
   $('.aboutTag').on('click', 'a', function () {
-        $('.aboutHeader').ScrollTo({
-            duration: 1000
-        });
+    $('html, body').animate({
+        scrollTop: $('.aboutHeader').offset().top }, 1000);
     });
 
-    $('.contactTag').on('click', 'a', function () {
-          $('.contact').ScrollTo({
-              duration: 1000
-          });
+  $('.contactTag').on('click', 'a', function () {
+    $('html, body').animate({
+        scrollTop: $(".contact").offset().top }, 1000);
     });
 
-    $('.downWrap').on('click', 'a', function () {
-          $('#work').ScrollTo({
-              duration: 1000
-          });
+  $('.downWrap').on('click', 'a', function () {
+    $('html, body').animate({
+        scrollTop: $("#work").offset().top }, 1000);
     });
 
 });
